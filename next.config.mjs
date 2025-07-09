77a649a0348141ab63d5af0a10f7144d87c288eb
Antoine Lelong <azenoxe@gmail.com>
2025-07-09 16:11:07 +0200

    feat: add payload with admin panel

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
+import { withPayload } from "@payloadcms/next/withPayload";
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -18,4 +19,4 @@ const config = {
   },
 };
 
-export default config;
+export default withPayload(config);
