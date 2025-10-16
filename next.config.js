c5ab643a4af35b96f89c366f9cda30f286e97c3b
tomdepussay <depussay.tom1@gmail.com>
2025-10-16 10:42:11 +0200

    fix: biome payload

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	images: {
-		domains: ["localhost"], // allow localhost
-	},
-};
-
-export default nextConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -38,6 +38,7 @@ const config = {
 				pathname: "/**",
 			},
 		],
+		domains: ["localhost"],
 	},
 };
 
