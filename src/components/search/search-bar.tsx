47651218f60ffa2cbeedbdc9a76834bf2708a8b7
tomdepussay <depussay.tom1@gmail.com>
2025-10-21 17:25:02 +0200

    feat: searchbar

--- src/components/search/search-bar.tsx
+++ src/components/search/search-bar.tsx
@@ -1,13 +1,75 @@
-import { Flex, Icon, IconButton, Input  } from "@chakra-ui/react";
-import { useEffect, useRef, useState } from "react";
+import {
+	Box,
+	Flex,
+	Icon,
+	IconButton,
+	Input,
+	Separator,
+	Spinner,
+	Text,
+} from "@chakra-ui/react";
+import { useEffect, useRef, useState, type KeyboardEvent } from "react";
 import { LuSearch, LuX } from "react-icons/lu";
-import { useDebounce } from "~/hooks/use-debounce";
+import { api } from "~/utils/api";
+import SearchCategory from "./search-category";
+import SearchTool from "./search-tool";
+import { IoMdWarning } from "react-icons/io";
 
 export default function SearchBar() {
 	const searchRef = useRef<HTMLDivElement>(null);
 	const [isFocus, setIsFocus] = useState(false);
 	const [search, setSearch] = useState("");
-	const debouncedSearch = useDebounce(search);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [searched, setSearched] = useState(false);
+	const [error, setError] = useState("");
+
+	const {
+		data: categories,
+		isLoading: isLoadingCategories,
+		refetch: refetchCategories,
+	} = api.category.getList.useQuery(
+		{
+			limit: 0,
+			filters: [{ key: "name", operation: "contains", value: searchTerm }],
+			sort: ["name"],
+		},
+		{ enabled: false },
+	);
+
+	const {
+		data: tools,
+		isLoading: isLoadingTools,
+		refetch: refetchTools,
+	} = api.tool.getList.useQuery(
+		{
+			limit: 0,
+			filters: [{ key: "name", operation: "contains", value: searchTerm }],
+			sort: ["privacy_score_saas", "dpa_compliant"],
+		},
+		{ enabled: false },
+	);
+
+	const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") {
+			handleSearch();
+		}
+	};
+
+	const handleSearch = async () => {
+		setError("");
+		if (search.length < 3) {
+			setError("Votre recherche doit comporter au moins 3 caractères.");
+		} else {
+			setSearched(true);
+			setSearchTerm(search);
+		}
+	};
+
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		refetchCategories();
+		refetchTools();
+	}, [searchTerm]);
 
 	useEffect(() => {
 		function handleClickOutside(event: MouseEvent) {
@@ -27,55 +89,136 @@ export default function SearchBar() {
 	}, []);
 
 	return (
-		<Flex
-			ref={searchRef}
-			alignItems="center"
-			bgColor={isFocus ? "blue.50" : "white"}
-			rounded="full"
-			px={4}
-			py={3}
-			mt={8}
-			w="full"
-			borderWidth={1}
-			borderColor={isFocus ? "blue.500" : "gray.100"}
-			transition="all 0.2s ease-in-out"
-		>
-			<Input
-				placeholder="Rechercher un outil ou une catégorie"
-				size="lg"
-				w="full"
-				variant="outline"
-				outline="none"
-				border="none"
-				onFocus={() => setIsFocus(true)}
-				value={search}
-				onChange={(e) => setSearch(e.target.value)}
-			/>
-			{search && (
-				<IconButton
-					aria-label="Annuler la recherche"
-					bgColor={isFocus ? "white" : "gray.100"}
-					rounded="full"
-					size="2xs"
-					p={0}
-					mr={4}
-					onClick={() => {
-						setSearch("");
-						setIsFocus(false);
-					}}
-				>
-					<Icon as={LuX} boxSize={3} color="blue.600" />
-				</IconButton>
+		<>
+			{isFocus && (
+				<Box
+					position="absolute"
+					bottom={0}
+					left={0}
+					w={"full"}
+					h={"full"}
+					bgGradient={"to-t"}
+					gradientFrom={"blue.800"}
+					gradientTo={"transparent"}
+					zIndex={"dropdown"}
+				/>
 			)}
-			<IconButton
-				aria-label="Rechercher un outil"
+			<Flex
+				ref={searchRef}
+				alignItems="center"
+				bgColor={isFocus ? "blue.50" : "white"}
 				rounded="full"
-				p={6}
-				flex={1}
-				colorPalette={isFocus ? "primary" : "black"}
+				px={4}
+				py={3}
+				mt={8}
+				w="full"
+				borderWidth={1}
+				borderColor={
+					isFocus ? (error === "" ? "blue.500" : "red.600") : "gray.100"
+				}
+				transition="all 0.2s ease-in-out"
+				zIndex={"sticky"}
 			>
-				<Icon as={LuSearch} boxSize={6} />
-			</IconButton>
-		</Flex>
+				<Input
+					placeholder="Rechercher un outil ou une catégorie"
+					size="lg"
+					w="full"
+					variant="outline"
+					outline="none"
+					border="none"
+					onFocus={() => setIsFocus(true)}
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+					onKeyDown={(e) => handleKeyDown(e)}
+				/>
+				{search && (
+					<IconButton
+						aria-label="Annuler la recherche"
+						bgColor={isFocus ? "white" : "gray.100"}
+						rounded="full"
+						size="2xs"
+						p={0}
+						mr={4}
+						onClick={() => {
+							setSearch("");
+							setIsFocus(false);
+							setSearched(false);
+						}}
+					>
+						<Icon as={LuX} boxSize={3} color="blue.600" />
+					</IconButton>
+				)}
+				<IconButton
+					aria-label="Rechercher un outil"
+					rounded="full"
+					p={6}
+					flex={1}
+					colorPalette={isFocus ? "primary" : "black"}
+					onClick={() => handleSearch()}
+				>
+					<Icon as={LuSearch} boxSize={6} />
+				</IconButton>
+			</Flex>
+			<Flex position={"relative"} w={"full"}>
+				{(searched || error !== "") && isFocus && (
+					<Flex
+						position={"absolute"}
+						flexDir={"column"}
+						w={"full"}
+						mt={1}
+						rounded={"3xl"}
+						borderWidth={1}
+						borderColor={"gray.100"}
+						p={5}
+						gap={5}
+						bgColor={"white"}
+						zIndex={"sticky"}
+					>
+						{error !== "" ? (
+							<Flex alignItems={"center"} gap={1}>
+								<Icon as={IoMdWarning} color={"red.600"} />
+								<Text color={"red.600"}>{error}</Text>
+							</Flex>
+						) : isLoadingCategories && isLoadingTools ? (
+							<Flex
+								w={"full"}
+								flexDir={"column"}
+								justifyContent={"center"}
+								alignItems={"center"}
+							>
+								<Spinner size={"xl"} color={"blue.600"} />
+								<Text>Chargement...</Text>
+							</Flex>
+						) : (
+							<>
+								{categories && categories.length > 0 && (
+									<>
+										<Text fontWeight={400} fontSize={16} color={"blue.600"}>
+											{categories.length} catégorie
+											{categories.length > 1 ? "s" : ""}
+										</Text>
+										{categories.map((category) => (
+											<SearchCategory key={category.id} category={category} />
+										))}
+										<Separator color={"gray.100"} />
+									</>
+								)}
+								{tools && tools.length > 0 && (
+									<>
+										<Text fontWeight={400} fontSize={16} color={"blue.600"}>
+											{tools.length} outil
+											{tools.length > 1 ? "s" : ""}
+										</Text>
+										{tools.map((tool) => (
+											<SearchTool key={tool.id} tool={tool} />
+										))}
+									</>
+								)}
+							</>
+						)}
+					</Flex>
+				)}
+			</Flex>
+		</>
 	);
 }
---
+++ src/components/search/search-category.tsx
@@ -0,0 +1,58 @@
+import { Link as ChakraLink, Flex, Text } from "@chakra-ui/react";
+import type { Category } from "~/payload/payload-types";
+import NextLink from "next/link";
+import CategoryIcon from "../ui/icons/category-icon";
+import Badge from "../ui/badge/badge";
+
+type Props = {
+	category: Category;
+};
+
+export default function SearchCategory({ category }: Props) {
+	return (
+		<ChakraLink
+			_hover={{ textDecoration: "none" }}
+			_focus={{ outline: "none" }}
+			asChild
+			w={"full"}
+		>
+			<NextLink href={`/categories/${category.id}`} passHref>
+				<Flex
+					borderWidth={1}
+					borderColor={"gray.100"}
+					rounded={"2xl"}
+					p={4}
+					gap={4}
+					w={"full"}
+				>
+					<Flex
+						w={20}
+						h={20}
+						rounded={"2.5xl"}
+						bgColor={`${category.color}.50`}
+						borderWidth={1}
+						borderColor={`${category.color}.100`}
+						justifyContent={"center"}
+						alignItems={"center"}
+					>
+						<CategoryIcon size={52} category={category} />
+					</Flex>
+					<Flex flexDir={"column"} w={"full"} gap={3}>
+						<Flex justifyContent={"space-between"}>
+							<Text fontSize={20} fontWeight={500}>
+								{category.name}
+							</Text>
+							<Badge>
+								{category.relatedTools?.docs?.length} outil
+								{(category.relatedTools?.docs?.length ?? 0) > 1 ? "s" : ""}
+							</Badge>
+						</Flex>
+						<Text fontSize={14} fontWeight={300} color={"gray.600"}>
+							{category.description}
+						</Text>
+					</Flex>
+				</Flex>
+			</NextLink>
+		</ChakraLink>
+	);
+}
