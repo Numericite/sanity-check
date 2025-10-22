9b25268f68909cd0686569daa625bad8e42a07b2
tomdepussay <depussay.tom1@gmail.com>
2025-10-22 10:19:01 +0200

    fix: optimisation and rename layout/navbar

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -5,9 +5,9 @@ import appChakraSystem from "~/utils/chakra-system";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
-  return (
-    <ChakraProvider value={appChakraSystem}>
-      <ColorModeProvider forcedTheme="light" {...props} />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider value={appChakraSystem}>
+			<ColorModeProvider forcedTheme="light" {...props} />
+		</ChakraProvider>
+	);
 }
