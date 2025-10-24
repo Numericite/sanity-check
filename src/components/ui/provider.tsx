96c0c1462448c2d2d8b82d0998087e8590bd2955
Antoine Lelong <azenoxe@gmail.com>
2025-10-24 12:18:42 +0200

    fix: add default export to chakra-system for cli issue

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import { appChakraSystem } from "~/utils/chakra-system";
+import appChakraSystem from "~/utils/chakra-system";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
-	return (
-		<ChakraProvider value={appChakraSystem}>
-			<ColorModeProvider forcedTheme="light" {...props} />
-		</ChakraProvider>
-	);
+  return (
+    <ChakraProvider value={appChakraSystem}>
+      <ColorModeProvider forcedTheme="light" {...props} />
+    </ChakraProvider>
+  );
 }
